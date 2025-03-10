eba0b14bb56679af4f0e658866ca22d1353052a5
heidevdav <hei.david.25@gmail.com>
2025-03-10 15:18:03 +0300

    [ADD] amélioration du fichier combinaison.js

--- combinaison.js
+++ combinaison.js
@@ -2,7 +2,11 @@ const factorielle = require("./factorielle.js");
 const operation = require("./factorielle.js");
 
 function combinaison(n, p) {
-    return (n>p)? (operation.factorielle(n))/(operation.factorielle(n-p)*operation.factorielle(p)):"n est inférieur à p donc impossible de faire le calcul de combinaison"
+    console.log('--------');
+    console.log(`n: ${n}`);
+    console.log(`p: ${p}`);
+    return (n>p)? `==> Factorielle : ${(operation.factorielle(n))/(operation.factorielle(n-p)*operation.factorielle(p))}`:"==> n est inférieur à p donc impossible de faire le calcul de combinaison"
 }
 
+console.log(combinaison(3, 2));
 console.log(combinaison(1, 2));
