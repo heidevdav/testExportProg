04167a9d5ad117445bc06b17029a57169b00996f
heidevdav <hei.david.25@gmail.com>
2025-03-10 15:11:08 +0300

    [ADD] modification du fichier combinaison.js

--- combinaison.js
+++ combinaison.js
@@ -1,3 +1,8 @@
+const factorielle = require("./factorielle.js");
 const operation = require("./factorielle.js");
 
-console.log(operation.factorielle(5));
+function combinaison(n, p) {
+    return (n>p)? (operation.factorielle(n))/(operation.factorielle(n-p)*operation.factorielle(p)):"n est inférieur à p donc impossible de faire le calcul de combinaison"
+}
+
+console.log(combinaison(1, 2));
